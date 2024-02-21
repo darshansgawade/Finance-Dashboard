14ff37d24c3f7c1b7fb7e220131b48c4c407435c
Darshan Sanjiv Gawade <146985773+darshansgawade@users.noreply.github.com>
2024-02-22 01:24:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,3 +19,35 @@ toggle.onclick = function () {
   navigation.classList.toggle("active");
   main.classList.toggle("active");
 };
+
+// Save data function
+
+function saveData() {
+  let name, email, tel, passward;
+  name = document.getElementById("name").value;
+  email = document.getElementById("email").value;
+  tel = document.getElementById("tel").value;
+  passward = document.getElementById("passward").value;
+
+  //console.log(name + email + tel + passward);
+
+  //localStorage.setItem("name", name);
+  //localStorage.setItem("email", email);
+  //localStorage.setItem("tel", tel);
+  //localStorage.setItem("passward", passward);
+
+  let user_records = JSON.parse(localStorage.getItem("users")) || []; // Corrected variable name and added default empty array
+  if (user_records.some((v) => {
+    return v.email === email && v.tel === tel; // Corrected variable name and used strict equality
+  })) {
+    alert("Duplicate Data");
+  } else {
+    user_records.push({
+      "name": name,
+      "email": email,
+      "tel": tel,
+      "passward": passward
+    });
+    localStorage.setItem("users", JSON.stringify(user_records));
+  }
+}
